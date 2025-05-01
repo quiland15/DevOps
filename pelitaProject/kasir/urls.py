e6ab73066a413a5427bbacbb85b1bd76aabc729d
quiland15 <quiland86@gmail.com>
2025-05-01 13:12:02 +0000

    finish login

--- pelitaProject/kasir/urls.py
+++ pelitaProject/kasir/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views.auth import login_view, logout_view
-from .views.dashboard import dashboard_view
-from .views.admin import admin_panel_view
-from .views.cashier import cashier_dashboard_view
+from .views.auth import login_view
+#from .views.dashboard import dashboard_view
+#from .views.admin import admin_panel_view
+#from .views.cashier import cashier_dashboard_view
 
 urlpatterns = [
     path("login/", login_view, name="login"),
--- pelitaProject/kasir/views/auth.py
+++ pelitaProject/kasir/views/auth.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
-from django.contrib import messages
+#from django.contrib.auth import authenticate, login, logout
+#from django.contrib import messages
 
 def login_view(request):
     context = {
