256ca30676842254d794a444c3c677831b48257e
quiland15 <quiland86@gmail.com>
2025-05-01 20:51:38 +0800

    login

--- pelitaProject/kasir/views/auth.py
+++ pelitaProject/kasir/views/auth.py
@@ -3,19 +3,25 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 def login_view(request):
-    if request.method == "POST":
-        user = authenticate(
-            request,
-            username=request.POST["username"],
-            password=request.POST["password"]
-        )
-        if user is not None:
-            login(request, user)
-            return redirect("dashboard")
-        else:
-            messages.error(request, "Login gagal.")
-    return render(request, "login.html")
+    context = {
+        'title':'Login - PELITAKAS',
+        'css':'css/login.css',
+    }
+    # if request.method == "POST":
+    #     user = authenticate(
+    #         request,
+    #         username=request.POST["username"],
+    #         password=request.POST["password"]
+    #     )
+    #     if user is not None:
+    #         login(request, user)
+    #         return redirect("dashboard")
+    #     else:
+    #         messages.error(request, "Login gagal.")
 
-def logout_view(request):
-    logout(request)
-    return redirect("login")
+    
+    return render(request, "kasir/index.html", context)
+
+# def logout_view(request):
+#     logout(request)
+#     return redirect("login")
