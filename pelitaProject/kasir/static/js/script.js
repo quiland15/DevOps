e7fcef82e261764021a38bb58caac3ebf260f471
quiland15 <quiland86@gmail.com>
2025-05-02 04:32:19 +0000

    Manage website

--- pelitaProject/kasir/static/js/script.js
+++ pelitaProject/kasir/static/js/script.js
@@ -4,26 +4,26 @@ function toggleSidebar() {
   }
 
   // Chart.js Example
-  const ctx = document.getElementById('salesChart').getContext('2d');
-  const salesChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun'],
-      datasets: [
-        {
-          label: 'Pertanian',
-          backgroundColor: '#0d6efd',
-          data: [10, 20, 30, 40, 50, 60]
-        },
-        {
-          label: 'Peternakan',
-          backgroundColor: '#f39c12',
-          data: [60, 50, 40, 30, 20, 10]
-        }
-      ]
-    },
-    options: {
-      responsive: true,
-      maintainAspectRatio: true
-    }
-  });
+const ctx = document.getElementById('salesChart').getContext('2d');
+ const salesChart = new Chart(ctx, {
+   type: 'bar',
+   data: {
+     labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun'],
+     datasets: [
+       {
+         label: 'Pertanian',
+         backgroundColor: '#0d6efd',
+         data: [10, 20, 30, 40, 50, 60]
+       },
+       {
+         label: 'Peternakan',
+         backgroundColor: '#f39c12',
+         data: [60, 50, 40, 30, 20, 10]
+       }
+     ]
+   },
+   options: {
+     responsive: true,
+     maintainAspectRatio: true
+   }
+ });
