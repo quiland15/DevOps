7d6162d3324c3be873e689e51392d69545e68e3c
quiland15 <quiland86@gmail.com>
2025-05-03 03:01:25 +0000

    cashier

--- pelitaProject/kasir/views/cashier.py
+++ pelitaProject/kasir/views/cashier.py
@@ -3,6 +3,4 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def cashier_dashboard_view(request):
-    if not request.user.groups.filter(name='Cashier').exists():
-        return render(request, 'login')
     return render(request, "kasir/kasir.html")
