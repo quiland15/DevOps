fec647dd9bcc8ddc71d2a936164fd237c28c2a3f
quiland15 <quiland86@gmail.com>
2025-05-06 21:42:19 +0800

    change

--- pelitaProject/kasir/views/dashboard.py
+++ pelitaProject/kasir/views/dashboard.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.db.models import Sum, Count
 from django.contrib.auth.decorators import login_required
 from kasir.models import Product, Transaction, TransactionItem, InventoryLog
-from django.utils.timezone import now
-from datetime import timedelta
+# from django.utils.timezone import now
+# from datetime import timedelta
 from django.db.models.functions import TruncMonth
 
 @login_required
@@ -15,33 +15,33 @@ def adminDashboard_view(request):
     low_stock_products = Product.objects.filter(stock__lte=2).order_by('stock')[:5]
 
 
-monthly_data = (
-    TransactionItem.objects
-    .select_related('product__category', 'transaction')
-    .annotate(month=TruncMonth('transaction__created_at'))
-    .values('month', 'product__category__name')
-    .annotate(total=Sum('quantity'))
-    .order_by('month')
-)
+    monthly_data = (
+        TransactionItem.objects
+        .select_related('product__category', 'transaction')
+        .annotate(month=TruncMonth('transaction__created_at'))
+        .values('month', 'product__category__name')
+        .annotate(total=Sum('quantity'))
+        .order_by('month')
+    )
 
-labels = []
-sales_pertanian = []
-sales_peternakan = []
+    labels = []
+    sales_pertanian = []
+    sales_peternakan = []
 
-for data in monthly_data:
-    month_str = data['month'].strftime('%b')  # contoh: Jan, Feb
-    if month_str not in labels:
-        labels.append(month_str)
-        sales_pertanian.append(0)
-        sales_peternakan.append(0)
+    for data in monthly_data:
+        month_str = data['month'].strftime('%b')  # contoh: Jan, Feb
+        if month_str not in labels:
+            labels.append(month_str)
+            sales_pertanian.append(0)
+            sales_peternakan.append(0)
 
-    index = labels.index(month_str)
-    category_name = data['product__category__name'].lower()
+        index = labels.index(month_str)
+        category_name = data['product__category__name'].lower()
 
-    if 'pertanian' in category_name:
-        sales_pertanian[index] += data['total']
-    elif 'peternakan' in category_name:
-        sales_peternakan[index] += data['total']
+        if 'pertanian' in category_name:
+            sales_pertanian[index] += data['total']
+        elif 'peternakan' in category_name:
+            sales_peternakan[index] += data['total']
 
     # Produk populer (dari TransactionItem)
     produk_populer = (
