ef08f247065a85d623c63fd31aa94fd9aa6714c9
quiland15 <quiland86@gmail.com>
2025-05-07 16:55:19 +0800

    change

--- pelitaProject/pelitaProject/settings.py
+++ pelitaProject/pelitaProject/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -50,7 +51,7 @@ ROOT_URLCONF = 'pelitaProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
