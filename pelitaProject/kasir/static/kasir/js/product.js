95875f6a742d497d91f32b2c41140afe059fcdd0
quiland15 <quiland86@gmail.com>
2025-05-13 01:42:46 +0800

    change sidebar

--- pelitaProject/kasir/static/kasir/js/product.js
+++ pelitaProject/kasir/static/kasir/js/product.js
@@ -58,6 +58,7 @@ async function toggleSidebar() {
 document.addEventListener("DOMContentLoaded", () => {
   fetchProducts();
   loadCategories();
+  document.getElementById("hamburgerBtn").addEventListener("click", toggleSidebar);
 });
   const productBody = document.getElementById("productBody");
   const btnSemua = document.getElementById("btn-semua");
