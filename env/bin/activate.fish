acc5ee01ba458b9fffc3204ddc55da3113646902
quiland15 <quiland86@gmail.com>
2025-05-15 02:09:37 +0000

    change env

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/quiland/DevOps/env
+set -gx VIRTUAL_ENV /var/www/html/DevOps/env
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
